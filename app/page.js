15e28e3d83fbc8101870976cbcddc3cfaa2ac0d7
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2024-03-05 07:53:16 -0800

    first commit

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="container mx-auto text-center text-lg">
+      سلام و صد سلام به شروع پروژه
+    </main>
+  );
+}
---
+++ components/header/index.jsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+const Header = () => {
+  return <div>header</div>;
+};
+
+export default Header;
